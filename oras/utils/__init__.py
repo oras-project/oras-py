31f1e7aa1fd1f905cab887ec6b54cc2ecfe2ad45
vsoch <vsoch@users.noreply.github.com>
2022-05-19 17:57:05 -0600

    isort
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- oras/utils/__init__.py
+++ oras/utils/__init__.py
@@ -1,18 +1,5 @@
-from .fileio import (
-    copyfile,
-    extract_targz,
-    get_file_hash,
-    get_size,
-    get_tmpdir,
-    get_tmpfile,
-    make_targz,
-    mkdir_p,
-    print_json,
-    read_file,
-    read_in_chunks,
-    read_json,
-    recursive_find,
-    write_file,
-    write_json,
-)
+from .fileio import (copyfile, extract_targz, get_file_hash, get_size,
+                     get_tmpdir, get_tmpfile, make_targz, mkdir_p, print_json,
+                     read_file, read_in_chunks, read_json, recursive_find,
+                     write_file, write_json)
 from .request import get_docker_client, iter_localhosts
