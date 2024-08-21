dfdab0f91190143ba09bd6615d28a6ccd20b50c1
Isabella do Amaral <idoamara@redhat.com>
2024-08-21 10:40:33 -0300

    test.sh: expose ORAS_PORT
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd $DIR/../
 
 # Ensure envars are defined - expected registry port and host
-export ORAS_PORT=5000
+export ORAS_PORT=${ORAS_PORT:-5000}
 export ORAS_HOST=localhost
 export ORAS_REGISTRY=${ORAS_HOST}:${ORAS_PORT}
 export ORAS_USER=myuser
