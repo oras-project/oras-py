c761fcfd2e5a911b0d9fc60febe1ba17f23ec0f5
Andrew Woodward <xarses@gmail.com>
2024-09-13 17:57:59 -0700

    Replace logger.exit(...) from the internals
    Replace with raise ValueError instead so that the caller has the option to
    handle the problem as they desire.
    
    Fixes: #154
    Signed-off-by: Andrew Woodward <xarses@gmail.com>

--- oras/auth/__init__.py
+++ oras/auth/__init__.py
@@ -1,7 +1,5 @@
 import requests
 
-from oras.logger import logger
-
 from .basic import BasicAuth
 from .token import TokenAuth
 
@@ -11,7 +9,7 @@ auth_backends = {"token": TokenAuth, "basic": BasicAuth}
 def get_auth_backend(name="token", session=None, **kwargs):
     backend = auth_backends.get(name)
     if not backend:
-        logger.exit(f"Authentication backend {backend} is not known.")
+        raise ValueError(f"Authentication backend {backend} is not known.")
     backend = backend(**kwargs)
     backend.session = session or requests.Session()
     return backend
