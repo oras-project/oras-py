d9bdeda6eaa6f3f48eaf7165d5c125037542742f
Andrew Woodward <xarses@gmail.com>
2024-09-16 19:01:04 -0700

    Add a dedicated Exemption for auth handlers
    In 4d248a8 we removed the sys.exit(...) but that now allows for auth failures
    to be retried by the decorators. Adding a dedicated Exemption so we don't have
    to retry on fatal auth-failures
    
    Signed-off-by: Andrew Woodward <xarses@gmail.com>

--- oras/auth/__init__.py
+++ oras/auth/__init__.py
@@ -6,6 +6,12 @@ from .token import TokenAuth
 auth_backends = {"token": TokenAuth, "basic": BasicAuth}
 
 
+class AuthenticationException(Exception):
+    """
+    An exception to traise with Authentication errors are fatal
+    """
+
+
 def get_auth_backend(name="token", session=None, **kwargs):
     backend = auth_backends.get(name)
     if not backend:
