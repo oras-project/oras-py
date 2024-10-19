18c964b3e9ffa72259b60dbcc9cca0a6ab32493e
tarilabs <matteo.mortari@gmail.com>
2024-10-19 16:27:06 +0200

    update changelog and version
    Signed-off-by: tarilabs <matteo.mortari@gmail.com>

--- oras/version.py
+++ oras/version.py
@@ -2,7 +2,7 @@ __author__ = "Vanessa Sochat"
 __copyright__ = "Copyright The ORAS Authors."
 __license__ = "Apache-2.0"
 
-__version__ = "0.2.22"
+__version__ = "0.2.23"
 AUTHOR = "Vanessa Sochat"
 EMAIL = "vsoch@users.noreply.github.com"
 NAME = "oras"
