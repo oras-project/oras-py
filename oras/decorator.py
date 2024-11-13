3ea4643e81160938ce62f78bc10203cc7d663140
Isabella do Amaral <idoamara@redhat.com>
2024-11-13 11:32:05 -0300

    decorator: get rid of inheritance
    Signed-off-by: Isabella do Amaral <idoamara@redhat.com>

--- oras/decorator.py
+++ oras/decorator.py
@@ -3,72 +3,36 @@ __copyright__ = "Copyright The ORAS Authors."
 __license__ = "Apache-2.0"
 
 import time
-from functools import partial, update_wrapper
+from functools import wraps
 
 import oras.auth
 from oras.logger import logger
 
 
-class Decorator:
-    """
-    Shared parent decorator class
-    """
-
-    def __init__(self, func):
-        update_wrapper(self, func)
-        self.func = func
-
-    def __get__(self, obj, objtype):
-        return partial(self.__call__, obj)
-
-
-class ensure_container(Decorator):
+def ensure_container(func):
     """
     Ensure the first argument is a container, and not a string.
     """
 
-    def __call__(self, cls, *args, **kwargs):
+    @wraps(func)
+    def wrapper(cls, *args, **kwargs):
         if "container" in kwargs:
             kwargs["container"] = cls.get_container(kwargs["container"])
         elif args:
             container = cls.get_container(args[0])
             args = (container, *args[1:])
-        return self.func(cls, *args, **kwargs)
+        return func(cls, *args, **kwargs)
+
+    return wrapper
 
 
-class classretry(Decorator):
-    """
-    Retry a function that is part of a class
-    """
-
-    def __init__(self, func, attempts=5, timeout=2):
-        super().__init__(func)
-        self.attempts = attempts
-        self.timeout = timeout
-
-    def __call__(self, cls, *args, **kwargs):
-        attempt = 0
-        attempts = self.attempts
-        timeout = self.timeout
-        while attempt < attempts:
-            try:
-                return self.func(cls, *args, **kwargs)
-            except oras.auth.AuthenticationException as e:
-                raise e
-            except Exception as e:
-                sleep = timeout + 3**attempt
-                logger.info(f"Retrying in {sleep} seconds - error: {e}")
-                time.sleep(sleep)
-                attempt += 1
-        return self.func(cls, *args, **kwargs)
-
-
-def retry(attempts, timeout=2):
+def retry(attempts=5, timeout=2):
     """
     A simple retry decorator
     """
 
     def decorator(func):
+        @wraps(func)
         def inner(*args, **kwargs):
             attempt = 0
             while attempt < attempts:
